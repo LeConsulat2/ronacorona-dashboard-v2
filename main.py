8d73585126c73955bf3016c943756c21452a28f2
LeConsulat2 <wooine2@gmail.com>
2024-07-01 23:34:31 +1200

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ st.markdown(
     <style>
     .css-1aumxhk{
         height: 30vh; /* Adjust this value as needed */
+        overflow-y: auto; /* Add scrolling if needed */
     }
     </style>
     """,
