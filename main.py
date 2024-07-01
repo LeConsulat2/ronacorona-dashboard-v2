7b4146d08e0b970ecff63d9303c4331537891100
LeConsulat2 <wooine2@gmail.com>
2024-07-01 23:27:29 +1200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ st.markdown(
     """
     <style>
     .css-1aumxhk{
-        height: 50vh; /* Adjust this value as needed */
+        height: 120vh; /* Adjust this value as needed */
     }
     </style>
     """,
