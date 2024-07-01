10a53c85aa08e038df8bfe8d07568ecc396f7c64
LeConsulat2 <wooine2@gmail.com>
2024-07-01 23:42:29 +1200

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ st.markdown(
     <style>
     /* Adjust the vh for the main container */
     .main .block-container {
-        height: 100vh; /* Adjust this value as needed */
-        max-height:100vh;
+        height: 90vh; /* Adjust this value as needed */
+        max-height:90vh;
         width: 100vw; /* Adjust this value as needed */
         max-width: 100vw /* Adjust this value as needed */
         overflow-y: auto; /* Add scrolling if needed */
